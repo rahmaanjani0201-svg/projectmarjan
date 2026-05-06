cfc78f04033b236efc7f75bc23aaedebb9aab5fd
marjan_app <rahmaanjani0201@gmail.com>
2026-05-06 10:50:52 +0700

    dfatra

--- lib/auth/select_page.dart
+++ lib/auth/select_page.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:project_marjan/auth/login_siswa.dart';
 
-class SelectPage extends StatefulWidget {
+class SelectPage extends StatelessWidget {
   const SelectPage({super.key});
 
   @override
-  State<SelectPage> createState() => _SelectPageState();
-}
-
-class _SelectPageState extends State<SelectPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pinkAccent,
       body: Container(
-        height: double.infinity,
         width: double.infinity,
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LoginSiswa()),
-            );
-          },
-          child:Image.asset("assets/images/murid.png"),
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Masuk sebagai",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  BoxShadow(
+                    offset: Offset(1, 1),
+                    color: Colors.black,
+                    blurRadius: 2,
+                    spreadRadius: 2,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 75),
+            ElevatedButton(
+              style: ,
+                onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => LoginSiswa(),));
+            }, child: Text("Siswa"))
+          ],
         ),
       ),
     );
